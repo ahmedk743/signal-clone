a52af1eef91bb7307d9d841209321989264b9b0e
Ahmed Khan <a.manzoor743@gmail.com>
2021-06-01 10:27:33 +0500

    all files

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA60IvWluQ72JAtWK7b2l3pBk6mrLc6caY",
+    authDomain: "signal-clone-3c5c3.firebaseapp.com",
+    projectId: "signal-clone-3c5c3",
+    storageBucket: "signal-clone-3c5c3.appspot.com",
+    messagingSenderId: "36875219109",
+    appId: "1:36875219109:web:9a89d6457cde9ea5651d4c"
+  };
